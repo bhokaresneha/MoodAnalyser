560de6d18a1932cbfc5d8b8e66bad3185f619b3d
bhokaresneha <bhokaresneha20@gmail.com>
2022-05-12 09:10:30 +0530

    Added : Returning HAPPY if User Provides Invalid Mood

--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -6,10 +6,27 @@ import org.junit.jupiter.api.Test;
 public class MoodAnalyserTest
 {
     @Test
-    public void givenMessage_WhenProper_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser=new MoodAnalyser();
-        String result=moodAnalyser.analyseMood("User is sad");
+    public void givenMessage_WhenProper_ShouldReturnSad()
+    {
+        MoodAnalyser moodAnalyser=new MoodAnalyser("I am in sad mood.");
+        String result=moodAnalyser.analyseMood();
         System.out.println(result);
         Assertions.assertEquals("SAD",result);
+    }
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnHappy()
+    {
+        MoodAnalyser moodAnalyser=new MoodAnalyser("I am in any mood.");
+        String result=moodAnalyser.analyseMood();
+        System.out.println(result);
+        Assertions.assertEquals("HAPPY",result);
+    }
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnNull()
+    {
+        MoodAnalyser moodAnalyser=new MoodAnalyser(null);
+        String result=moodAnalyser.analyseMood();
+        System.out.println(result);
+        Assertions.assertEquals("HAPPY (Invalid Mood block)",result);
     }
 }
