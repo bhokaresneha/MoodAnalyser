a16cc596b30e180ab17a059ec6f758f287a45802
bhokaresneha <bhokaresneha20@gmail.com>
2022-05-12 08:09:16 +0530

    Added :Given Message Return HAPPY

--- src/main/java/com/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -1,10 +1,16 @@
 package com.moodanalyser;
 public class MoodAnalyser {
-    public String analyseMood(String msg){
-        if(msg.contains("sad"))
+    public String analyseMood(String msg)
+    {
+        if (msg.contains("sad"))
         {
             return "SAD";
+        } else if (msg.contains("any"))
+        {
+            return "HAPPY";
+        } else
+        {
+            return null;
         }
-        return null;
     }
 }
